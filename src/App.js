1913320ca1dad7b5d768805a6f119adbd4d8dfc9
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-03 22:45:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Route,Routes} from "react-router-dom"
 import Layout from "./Components/Header/Layout";
 import Projects from "./Components/Projects/Projects";
 import Experience from "./Components/Experience/Experience";
+import About from "./Components/About/About";
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/" element={<About />} />
         <Route path='/Projects' element={<Projects />} />
         <Route path='/Experience' element={<Experience />} />
       </Routes>
