fed39d2bc4ae1c71b655b272bddbc04bd76c6930
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-04 11:30:20 +0530

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -27,8 +27,15 @@ const About=()=>{
             <p>What sets me apart is my ability to collaborate effectively within cross-functional teams. I thrive in a dynamic and creative environment where I can work alongside designers, backend developers, and other stakeholders to turn ideas into functional and beautiful web applications.</p>
             <p>In addition to my technical skills, I bring a strong work ethic, attention to detail, and a passion for problem-solving to every project. I understand the importance of responsive design, accessibility, and performance optimization, and I'm committed to delivering top-notch solutions that meet and exceed user expectations.</p>
             <p>I'm excited about the prospect of contributing to innovative projects and making a positive impact in the world of frontend development. I look forward to the opportunities and challenges that lie ahead and am eager to continue learning and growing as a frontend developer.</p>
+            <p>Skills: Html , Css , Javascript , ReactJs , React-Router, Redux, Redux-Toolkit<br />
+            Tools/Fromeworks: Firebase, Bootstrap, Virtual Studio Code, Github</p>
             <button className="btn btn-right" onClick={ShowProjectsHandler}>My Projects</button>
             <button className="btn btn-right" onClick={ShowExperienceHandler}>Work Experience</button>
+            </div>
+            <div className="contact-info"> 
+              <h3>Contact info:</h3>
+              <p>Email: vaibhavlal0211@gmail.com</p>
+              <p>Phone Number: (+91) 6200713519</p>
             </div>
           </section>
         </React.Fragment>
